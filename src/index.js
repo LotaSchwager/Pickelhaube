9eec021618c5f9feacd43aefe0db130ca8d0d497
LotaSchwager <ademir.munoz.r@mail.pucv.cl>
2023-04-13 21:00:57 -0400

    se añadio otro commando

--- src/index.js
+++ src/index.js
@@ -14,12 +14,19 @@ client.on('ready', (c) => {
    console.log(`${c.user.tag} esta conectado!!`);
 });
 
-client.on('messageCreate', (msg) => {
-   if (msg.author.bot){return;}
+client.on('interactionCreate', (interaction) => {
+   if (!interaction.isChatInputCommand())return;
 
-   if (msg.content.substring(0,1) === '/'){
-       if(msg.content === '/hola'){msg.reply(`Aweonao ${msg.author}`);}
+   if (interaction.commandName === 'hola'){interaction.reply(`${interaction.member} aweonao`);}
+
+   if (interaction.commandName === 'versus'){
+      const var1 = interaction.options.get('first-user').value;
+      const var2 = interaction.options.get('second-user').value;
+
+      interaction.reply(`${var1} se agarra a combos con ${var2}`);
    }
+
+    
 });
 
 client.login(process.env.TOKEN);
--- src/register-commands.js
+++ src/register-commands.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { Routes } = require('discord.js');
+const { Routes, ApplicationCommandOptionType } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
@@ -8,13 +8,31 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 const commands = [
     {
         name: 'hola',
-        description: 'Te dice aweonao',
+        description: 'Te insulta',
+    },
+    {
+        name: 'versus',
+        description: 'usuario vs usuario',
+        options: [
+            {
+                name: 'first-user',
+                description: 'El primer peleador',
+                type: ApplicationCommandOptionType.String,
+                required: true,
+            },
+            {
+                name: 'second-user',
+                description: 'El segundo peleador',
+                type:  ApplicationCommandOptionType.String,
+                require: true,
+            },
+        ],
     },
 ];
 
 (async () => {
     try{
-        console.log("Empieza");
+        console.log("Registrando los comandos '/'");
 
         await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID,process.env.GUILD_ID),
         { body: commands }
